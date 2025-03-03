c139cd9149630079bcdc1b01f056447f2ade10b0
Lucas Hearn <lhearnwork@gmail.com>
2025-03-03 12:24:32 -0500

    Create DataLoaderV1 and GPTDatasetV1 class and test output

---
+++ DataLoader/GPTV1/CreateDataLoaderV1.py
@@ -0,0 +1,3 @@
+import tiktoken
+from GPTDatasetV1 import GPTDatasetV1
+from torch.utils.data import DataLoader
---
+++ DataLoader/GPTV1/CreateDataLoaderV1Test.py
@@ -0,0 +1,13 @@
+from GPTDatasetV1 import create_data_loader_v1
+
+with open("LLM/Data/the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+dataloader = create_data_loader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+data_iterator = iter(dataloader)
+first_batch = next(data_iterator)
+second_batch = next(data_iterator)
+
+
+print('First Batch : ' , first_batch) 
+print('Second Batch : ' , second_batch) 
