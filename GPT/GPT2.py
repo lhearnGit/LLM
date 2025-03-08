64a775fa2d41393f06cfc1306604312bf5e0daf0
Lucas Hearn <lhearnwork@gmail.com>
2025-03-08 10:46:01 -0500

    Add Sample Text Generation, Untrained

--- GPT/GPT2.py
+++ GPT/GPT2.py
@@ -1,6 +1,8 @@
 import torch
 import tiktoken
 from GPT2Model import GPT2Model, GPT_CONFIG_124M
+
+
 torch.manual_seed(123)
 model = GPT2Model(GPT_CONFIG_124M)
 
@@ -21,22 +23,83 @@ print("\nOutput shape:", out.shape)
 print(out)
 
 
-
-total_params = sum(p.numel() for p in model.parameters())
-print(f"Total number of parameters: {total_params:,}")
-
-
-print("Token embedding layer shape:", model.token_emb.weight.shape)
-print("Output layer shape:", model.out_head.weight.shape)
+# Print test Statements 
+# total_params = sum(p.numel() for p in model.parameters())
+# print(f"Total number of parameters: {total_params:,}")
 
 
-total_params_gpt2 =  total_params - sum(p.numel() for p in model.out_head.parameters())
-print(f"Number of trainable parameters considering weight tying: {total_params_gpt2:,}")
+# print("Token embedding layer shape:", model.token_emb.weight.shape)
+# print("Output layer shape:", model.out_head.weight.shape)
 
-# Calculate the total size in bytes (assuming float32, 4 bytes per parameter)
-total_size_bytes = total_params * 4
 
-# Convert to megabytes
-total_size_mb = total_size_bytes / (1024 * 1024)
+# total_params_gpt2 =  total_params - sum(p.numel() for p in model.out_head.parameters())
+# print(f"Number of trainable parameters considering weight tying: {total_params_gpt2:,}")
 
-print(f"Total size of the model: {total_size_mb:.2f} MB")
+# # Calculate the total size in bytes (assuming float32, 4 bytes per parameter)
+# total_size_bytes = total_params * 4
+
+# # Convert to megabytes
+# total_size_mb = total_size_bytes / (1024 * 1024)
+
+# print(f"Total size of the model: {total_size_mb:.2f} MB")
+
+
+
+
+
+
+
+
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    # idx is (batch, n_tokens) array of indices in the current context
+    for _ in range(max_new_tokens):
+        
+        # Crop current context if it exceeds the supported context size
+        # E.g., if LLM supports only 5 tokens, and the context size is 10
+        # then only the last 5 tokens are used as context
+        idx_cond = idx[:, -context_size:]
+        
+        # Get the predictions
+        with torch.no_grad():
+            logits = model(idx_cond)
+        
+        # Focus only on the last time step
+        # (batch, n_tokens, vocab_size) becomes (batch, vocab_size)
+        logits = logits[:, -1, :]  
+
+        # Apply softmax to get probabilities
+        probas = torch.softmax(logits, dim=-1)  # (batch, vocab_size)
+
+        # Get the idx of the vocab entry with the highest probability value
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)  # (batch, 1)
+
+        # Append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+
+    return idx
+
+
+start_context = "Hello, I am"
+
+encoded = tokenizer.encode(start_context)
+print("encoded:", encoded)
+
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+print("encoded_tensor.shape:", encoded_tensor.shape)
+
+model.eval() # disable dropout
+
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor, 
+    max_new_tokens=6, 
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+
+print("Output:", out)
+print("Output length:", len(out[0]))
+
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
